ea70ecc94a101d2299bcd678f39705f93e8050bf
Kshitij Halmare <kshitij.halmare1@gmail.com>
2026-03-25 13:04:41 +0530

    Added Spring Boot project

---
+++ src/test/java/com/example/Gold_Wallet/GoldWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gold_Wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
